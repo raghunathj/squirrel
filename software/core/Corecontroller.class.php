0a6c425f939f7f137592e5fd203826c98d92b0f5
Raghunath J <jrcbangalore@gmail.com>
2013-03-13 09:07:11 +0530

    Core Controller

---
+++ software/core/Corecontroller.class.php
@@ -0,0 +1,30 @@
+<?php
+//The Core COntroller class file, this gets extened to all controller files in application
+//@author - Raghunath J
+//@path - /core/Corecontroller.class.php
+//version 1.0
+abstract class Corecontroller{
+
+	function __construct(){
+
+	}
+
+	//To display a view with/without data
+	public function view($view,$data = null){
+		$view_path = SITE_ROOT.'/application/views/'.$view.'.php';
+		//Check if file is present
+		if(file_exists($view_path)){
+			//Check if the data is not null.
+			//If its not null then extract the data to its own variables.
+			//More info about extract: http://php.net/manual/en/function.extract.php
+			if($data!=null){
+				extract($data);
+			}
+		}else{
+			//Include 404 page inside.
+			include SITE_ROOT.'/application/error/404.php';
+		}
+		return '';
+	}
+
+}
