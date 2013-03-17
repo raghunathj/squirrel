0407c5e421cdec7130fe3b2243118c3e2f8c3e9b
Raghunath J <jrcbangalore@gmail.com>
2013-03-17 15:41:15 +0530

    empty again

--- software/helpers/template.inc.php
+++ software/helpers/template.inc.php
@@ -1,1 +1,11 @@
 <?php
+//All the templating and config calling code is added here
+//@author - Raghunath J
+//Version 1.0
+//@path - /software/helpers/templating.inc.php
+
+//Get squirrel config options
+function get_option($name) {
+	global $sq_options;
+	return $sq_options[$name];
+}
