eee58b969c5d2eb2a9e1587de7c424e3ff23d02f
Raghunath J <jrcbangalore@gmail.com>
2013-03-17 15:42:18 +0530

    console_log code update

--- software/helpers/logging.inc.php
+++ software/helpers/logging.inc.php
@@ -4,3 +4,12 @@
 //@path - /software/helpers/logging.inc.php
 //version 1.0
 
+define('LOG_FILE','');
+define('LOG_MODE','');
+define('LOG_MODE_DEBUG','debug');
+define('LOG_MODE_WARNING','warning');
+define('LOG_MODE_ERROR','error');
+
+function console_log($message) {
+    error_log($message, 3, get_option(LOG_FILE));
+}
